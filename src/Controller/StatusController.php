3ac54d15fcdc3b8b69dab0a7e667c006dc98376a
Martijn Braam <martijn@brixit.nl>
2018-10-29 12:37:57 +0100

    Removed old code

--- src/Controller/StatusController.php
+++ src/Controller/StatusController.php
@@ -37,20 +37,6 @@ class StatusController extends AbstractController
     }
 
     /**
-     * @Route("/", name="status")
-     */
-    public function index()
-    {
-        $queue = $this->getDoctrine()->getRepository('App:Queue');
-        $queued = $queue->findBy(['status' => ['WAITING', 'BUILDING']], ['id' => 'DESC']);
-        $done = $queue->findBy(['status' => ['DONE', 'FAILED', 'SUPERSEDED']], ['id' => 'DESC'], 50);
-        return $this->render('status/index.html.twig', [
-            'queued' => $queued,
-            'done' => $done
-        ]);
-    }
-
-    /**
      * @Route("/log", name="log")
      */
     public function log()
