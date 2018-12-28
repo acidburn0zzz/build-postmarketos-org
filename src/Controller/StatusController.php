57d0712b03883e2a22f3759021986fc7c55d75df
Martijn Braam <martijn@brixit.nl>
2018-12-28 20:12:25 +0100

    Generate graph for commit

--- src/Controller/StatusController.php
+++ src/Controller/StatusController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class StatusController extends AbstractController
@@ -34,6 +35,47 @@ class StatusController extends AbstractController
             'queued' => $queued,
             'done' => $done
         ]);
+    }
+
+    /**
+     * @Route("/commits/{ref}", name="commit_details")
+     */
+    public function commitDetails($ref)
+    {
+        $commits = $this->getDoctrine()->getRepository('App:Commit');
+        $commit = $commits->findOneBy(['ref' => $ref]);
+        $packages = $commit->getPackages();
+
+        $graph = 'digraph g {' . PHP_EOL . '    rankdir=LR;' . PHP_EOL;
+        foreach ($packages as $package) {
+            $label = $package->getAport() . ' ' . $package->getPkgver() . '-r' . $package->getPkgrel();
+            switch ($package->getStatus()) {
+                case "WAITING":
+                    $color = 'azure1';
+                    break;
+                case "BUILDING":
+                    $color = 'azure3';
+                    break;
+                case "FAILED":
+                    $color = 'tomato';
+                    break;
+                case "DONE":
+                    $color = 'darkolivegreen1';
+                    break;
+                case "SUPERSEDED":
+                    $color = 'blanchedalmond'; //because why not
+                    break;
+            }
+            $graph .= '    ' . $package->getAport() . '[label="' . $label . '" fillcolor=' . $color . ']' . PHP_EOL;
+        }
+        $graph .= PHP_EOL . PHP_EOL;
+        foreach ($packages as $package) {
+            foreach ($package->getQueueDependencies() as $dependency) {
+                $graph .= '    ' . $dependency->getQueueItem()->getAport() . ' -> ' . $package->getAport() . PHP_EOL;
+            }
+        }
+        $graph .= ' }';
+        return new Response($graph);
     }
 
     /**
