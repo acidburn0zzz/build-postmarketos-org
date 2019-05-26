904c8ad0f1d54798902ec8f11e399f2868653433
Oliver Smith <ollieparanoid@bitmessage.ch>
2019-05-26 03:25:35 +0200

    Put package.depends in db layout

--- bpo/db/__init__.py
+++ bpo/db/__init__.py
@@ -3,8 +3,8 @@
 """ Database code, using sqlalchemy ORM.
     Usage example:
         session = bpo.db.session()
-        log_entry = bpo.db.Log(action="db_init", details="hello world")
-        session.add(log_entry)
+        log = bpo.db.Log(action="db_init", details="hello world")
+        session.add(log)
         session.commit() """
 
 import glob
@@ -16,7 +16,8 @@ import json
 import sqlalchemy
 import sqlalchemy.orm
 import sqlalchemy.ext.declarative
-from sqlalchemy import Column, Integer, String, DateTime, Text, ForeignKey
+from sqlalchemy import Column, Integer, String, DateTime, Text, ForeignKey, \
+                       Table
 from sqlalchemy.orm import relationship
 
 import bpo.config.args
@@ -48,13 +49,6 @@ class Package(base):
     repo = Column(String)
 
 
-class PackageDependency(base):
-    __tablename__ = "package_dependency"
-    id = Column(Integer, primary_key=True)
-    package_id = Column(Integer)
-    dependency_id = Column(Integer)
-
-
 class Queue(base):
     __tablename__ = "queue"
     id = Column(Integer, primary_key=True)
@@ -79,15 +73,29 @@ class Log(base):
 def init_relationships():
     self = sys.modules[__name__]
 
-    # log - n:1 - push
+    # commits.push_id - n:1 - push.id
+    self.Commit.push = relationship("Push", back_populates="commits")
+    self.Push.commits = relationship("Commit", order_by=self.Commit.id,
+                                     back_populates="push")
+
+    # log.push_id - n:1 - push.id
     self.Log.push = relationship("Push", back_populates="logs")
     self.Push.logs = relationship("Log", order_by=self.Log.id,
                                   back_populates="push")
 
-    # commits - n:1 - push
-    self.Commit.push = relationship("Push", back_populates="commits")
-    self.Push.commits = relationship("Commit", order_by=self.Commit.id,
-                                     back_populates="push")
+    # package.depends - n:n - package.required_by
+    # See "Self-Referential Many-to-Many Relationship" in:
+    # https://docs.sqlalchemy.org/en/13/orm/join_conditions.html
+    assoc = Table("package_dependency", base.metadata,
+                  Column("package_id", ForeignKey("package.id"),
+                         primary_key=True),
+                  Column("dependency_id", ForeignKey("package.id"),
+                         primary_key=True))
+    self.Package.depends = relationship("Package", secondary=assoc,
+        primaryjoin=self.Package.id==assoc.c.package_id,
+        secondaryjoin=self.Package.id==assoc.c.dependency_id,
+        order_by=self.Package.id,
+        backref="required_by")
 
 
 def init():
