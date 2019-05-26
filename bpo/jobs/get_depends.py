19e9bd6127c6592d7bdca18d586a5896d5563e20
Oliver Smith <ollieparanoid@bitmessage.ch>
2019-05-26 02:11:04 +0200

    Get my feet wet with sqlalchemy ORM
    Create basic database structure, and fill it from the gitlab push hook
    callback. Add an example gitlab push hook json data file for reference,
    and adjust the json code in the curl script to be more like what gitlab
    sends (so the commits have the right structure). Remove obsolete
    README.md. Various refactoring.

--- bpo/jobs/get_depends.py
+++ bpo/jobs/get_depends.py
@@ -3,15 +3,16 @@
 import bpo.helpers.job
 
 
-def run():
+def run(push_id):
     bpo.helpers.job.run("get_depends", {
         "hello": """
             echo "hello world"
             """,
         "pmbootstrap repo_missing": """
-            ./pmbootstrap.py repo_missing > repo_missing.json
+            # ./pmbootstrap.py repo_missing > repo_missing.json
             """,
         "submit": """
+            # TODO: use push_id: """ + str(push_id) + """
             pmaports/.sr.ht/submit.py --json task-submit repo_missing.json
             """,
     })
