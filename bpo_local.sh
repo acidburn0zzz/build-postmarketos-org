3b893843030690785943970ca85558d68946ab06
Oliver Smith <ollieparanoid@postmarketos.org>
2019-10-03 01:44:48 +0200

    bpo_local.sh: update
    Use -a and don't set a postmarketOS mirror, so it builds everything
    right now.

--- bpo_local.sh
+++ bpo_local.sh
@@ -1,6 +1,4 @@
 #!/bin/sh -ex
 rm -rf bpo.db _repo* _temp _html_out || true
 
-# Use old binary repository URL for testing locally, because otherwise it would
-# try to build *all* packages.
-./bpo.py -t test/test_tokens.cfg -m "http://postmarketos.brixit.nl" local
+./bpo.py -a -t test/test_tokens.cfg local
