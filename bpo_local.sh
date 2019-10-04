0fa73439888887bda25a5903bf34ffca974407ba
Oliver Smith <ollieparanoid@postmarketos.org>
2019-10-04 17:58:21 +0200

    bpo_local.sh: only run get_repo_missing if no db
    If there is already a database, it is more interesting to see how the
    code would continue building everything in the queue.

--- bpo_local.sh
+++ bpo_local.sh
@@ -1,3 +1,10 @@
 #!/bin/sh -ex
 
-./bpo.py -a -t test/test_tokens.cfg local
+args="-t test/test_tokens.cfg"
+
+# Fill DB with missing packages on first run
+if ! [ -f "./bpo.db" ]; then
+    args="$args --auto-get-repo-missing"
+fi
+
+./bpo.py $args local
