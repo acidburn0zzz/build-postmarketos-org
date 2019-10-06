bc536491312d095f4096dab6e7015445317d71c8
Oliver Smith <ollieparanoid@postmarketos.org>
2019-10-06 16:42:16 +0200

    sourcehut: format yaml properly and return job id

--- bpo/job_services/sourcehut.py
+++ bpo/job_services/sourcehut.py
@@ -14,7 +14,7 @@ from bpo.job_services.base import JobService
 
 def api_request(path, payload):
     url = "https://builds.sr.ht/api/" + path
-    headers = {"Authorization": bpo.config.tokens.sourcehut}
+    headers = {"Authorization": "token " + bpo.config.tokens.sourcehut}
     ret = requests.post(url, headers=headers, json=payload)
     logging.debug("sourcehut response: " + ret.text)
     if not ret.ok:
@@ -25,29 +25,41 @@ def api_request(path, payload):
 def get_manifest(tasks):
     manifest = {"image": "alpine/latest",
                 "packages": ["coreutils", "py3-requests"],
-                "sources": ["https://gitlab.com/postmarketOS/pmaports"],
+                "sources": ["https://gitlab.com/postmarketOS/pmaports.git/"],
                 "environment": {},
-                "secrets": [],
-                "tasks": tasks}
-    return yaml.dump(manifest)
+                "secrets": []}
+    ret = yaml.safe_dump(manifest)
+
+    # Pyyaml's safe_dump chokes on ordereddicts, so format tasks manually. This
+    # also makes sure that the formatting looks good, and is not in a single.
+    ret += "tasks:\n"
+    for name, script in tasks.items():
+        script_indented = "   " + script.replace("\n", "\n   ")
+        ret += "- {}: |\n{}".format(name, script_indented) + "\n"
+
+    return ret
 
 
 class SourcehutJobService(JobService):
     def script_setup(self, branch=None):
+        # TODO: install pmbootstrap
+        # TODO: yes "" | pmbootstrap init
         return """
-            # TODO: install pmbootstrap
-            # TODO: yes | pmbootstrap init
+            env
         """
 
     def run_job(self, name, tasks, branch=None):
         note = "WIP testing new bpo run job code"
-        result = api_request("jobs", {"manifest": get_manifest(tasks),
+        manifest = get_manifest(tasks)
+        print(manifest)
+        result = api_request("jobs", {"manifest": manifest,
                                       "note": note,
                                       "tags": [name],
                                       "execute": True,
                                       "secrets": True})
-        print(result)  # FIXME: extract job ID from result and return it
-        return 0
+        job_id = result.json()["id"]
+        logging.info("Job successfully started, got id: " + str(job_id))
+        return job_id
 
     def get_status(self, job_id):
         # TODO: get status from sourcehut
