e2d090064e1466777007976346e98c54a05935cb
Oliver Smith <ollieparanoid@postmarketos.org>
2019-10-06 12:08:26 +0200

    sourcehut: generate manifest and try to submit job
    Add new PyYAML dependency. In theory, this should submit a job to
    sourcehut and start a job. Untested, because I did not realize that one
    needs to add "token " before the actual token (will be added in
    following commits).

--- bpo/job_services/sourcehut.py
+++ bpo/job_services/sourcehut.py
@@ -1,14 +1,37 @@
 # Copyright 2019 Oliver Smith
 # SPDX-License-Identifier: AGPL-3.0-or-later
+""" Job service for builds.sr.ht, see: https://man.sr.ht/builds.sr.ht """
 
 import logging
 import os
 import requests
 import shlex
+import yaml
 
 import bpo.config.args
+import bpo.config.tokens
 import bpo.db
 from bpo.job_services.base import JobService
+
+
+def api_request(path, payload):
+    url = "https://builds.sr.ht/api/" + path
+    headers = {"Authorization": bpo.config.tokens.sourcehut}
+    ret = requests.post(url, headers=headers, json=payload)
+    logging.debug("sourcehut response: " + ret.text)
+    if not ret.ok:
+        raise RuntimeError("sourcehut API request failed: " + url)
+    return ret
+
+
+def get_manifest(tasks):
+    manifest = {"image": "alpine/latest",
+                "packages": ["coreutils", "py3-requests"],
+                "sources": ["https://gitlab.com/postmarketOS/pmaports"],
+                "environment": {},
+                "secrets": [],
+                "tasks": tasks}
+    return yaml.dump(manifest)
 
 
 class SourcehutJobService(JobService):
@@ -19,8 +42,14 @@ class SourcehutJobService(JobService):
         """
 
     def run_job(self, name, tasks, branch=None):
-       # TODO: send job to sourcehut and return the ID
-       return 0
+        note = "WIP testing new bpo run job code"
+        result = api_request("jobs", {"manifest": get_manifest(tasks),
+                                     "note": note,
+                                     "tags": [name],
+                                     "execute": True,
+                                     "secrets": True})
+        # FIXME: extract job ID from result and return it
+        return 0
 
     def get_status(self, job_id):
         # TODO: get status from sourcehut
