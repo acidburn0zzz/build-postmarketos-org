68edbd1935cb560fe8c448d42a7674ee96bbb0f1
Oliver Smith <ollieparanoid@postmarketos.org>
2019-10-09 22:14:33 +0200

    helpers/bpo_package_status.py: add
    Simple script for setting and getting the package status from the bpo
    sqlite database. See "bpo_package_status.py -h" for details.
    
    usage: bpo_package_status.py [-h] [-a ARCH] [-b BRANCH] [-d DB_PATH]
                                 (-g | -s {building,built,failed,published,queued})
                                 [pkgname [pkgname ...]]
    
    Closes: #44

--- .ci/flake8.sh
+++ .ci/flake8.sh
@@ -4,6 +4,7 @@ topdir="$(realpath "$(dirname "$0")/..")"
 flake8 \
 	*.py \
 	$(find bpo -name "*.py") \
+	$(find helpers -name "*.py") \
 	$(find test -name "*.py")
 
 echo "flake8 check passed"
---
+++ helpers/bpo_package_status.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python3
+# Copyright 2019 Oliver Smith
+# SPDX-License-Identifier: AGPL-3.0-or-later
+""" Simple tool to set and get package status (#44) """
+import argparse
+import os
+import sys
+
+# Add topdir to import path
+topdir = os.path.realpath(os.path.join(os.path.dirname(__file__) + "/.."))
+sys.path.insert(0, topdir)
+
+# Use "noqa" to ignore "E402 module level import not at top of file"
+import bpo.db  # noqa
+import bpo.config.args  # noqa
+
+
+def status_choices():
+    ret = []
+    for status in bpo.db.PackageStatus:
+        ret += [status.name]
+    return sorted(ret)
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("pkgnames", nargs="*", help="relevant package name"
+                        " (default: all)", metavar="pkgname")
+    parser.add_argument("-a", "--arch", default="x86_64",
+                        help="package architecture (default: x86_64)")
+    parser.add_argument("-b", "--branch", default="master",
+                        help="pmaports.git branch (default: master)")
+    parser.add_argument("-d", "--db-path", help="path to sqlite3 database",
+                        default=bpo.config.args.db_path)
+
+    # Get or set
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument("-g", help="get current status value", dest="get",
+                       action="store_true")
+    group.add_argument("-s", help="set new status value", dest="status",
+                       choices=status_choices())
+
+    return parser.parse_args()
+
+
+def get_all_pkgnames(session, arch, branch):
+    ret = []
+    for package in session.query(bpo.db.Package).filter_by(arch=arch,
+                                                           branch=branch):
+        ret += [package.pkgname]
+    return sorted(ret)
+
+
+def confirm(statement):
+    print(statement)
+    answer = input("Are you sure? [y/N] ")
+    if answer != "y":
+        print("Not answered with 'y', aborting!")
+        sys.exit(1)
+
+
+def set_status(session, pkgnames, arch, branch, status):
+    confirm("Will change status to '" + status + "' for the following"
+            " packages: " + str(pkgnames))
+
+    status = bpo.db.PackageStatus[status]
+    for pkgname in pkgnames:
+        package = bpo.db.get_package(session, pkgname, arch, branch)
+        bpo.db.set_package_status(session, package, status)
+
+    print("done!")
+    print()
+
+
+def get_status(session, pkgnames, arch, branch):
+    format_str = "{:8s} | {}"
+    print(format_str.format("status", "pkgname"))
+    print("-" * 20)
+    for pkgname in pkgnames:
+        package = bpo.db.get_package(session, pkgname, arch, branch)
+        print(format_str.format(package.status.name, package.pkgname))
+
+
+def main():
+    # Parse arguments
+    args = parse_arguments()
+    pkgnames = args.pkgnames
+    arch = args.arch
+    branch = args.branch
+
+    # Initialize db
+    bpo.config.args.db_path = args.db_path
+    bpo.db.init()
+    session = bpo.db.session()
+
+    if not pkgnames:
+        pkgnames = get_all_pkgnames(session, arch, branch)
+
+    if args.status:
+        set_status(session, pkgnames, arch, branch, args.status)
+
+    get_status(session, pkgnames, arch, branch)
+
+
+if __name__ == "__main__":
+    sys.exit(main())
