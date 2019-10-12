acidburn0zzz/build-postmarketos-org

commit 106799fa6803ececd1fe2c132b95f2ab7ae69d9c

callback/get_repo_missing: updated pkg -> queued
Change the status for updated packages to queued. Add a test.

Fixes: #43

bpo/api/job_callback/get_repo_missing.py
@@ -47,6 +47,7 @@ def update_or_insert_packages(session, payload, arch, branch):
                 bpo.jobs.build_package.abort(package_db)
             package_db.version = version
             package_db.repo = repo
+            package_db.status = bpo.db.PackageStatus.queued
         else:
             package_db = bpo.db.Package(arch, branch, pkgname, version)
         session.merge(package_db)

test/test_job_callback_get_repo_missing.py
@@ -6,6 +6,43 @@ import bpo.jobs
 import bpo.repo
 
 
+def test_callback_repo_missing_update_package(monkeypatch):
+    # Stop bpo server after bpo.repo.build was called 2x
+    global stop_count
+    stop_count = 0
+
+    def stop_count_increase(*args, **kwargs):
+        global stop_count
+        stop_count += 1
+        print("stop_count_increase: " + str(stop_count))
+        if stop_count == 2:
+            bpo_test.finish()
+    monkeypatch.setattr(bpo.repo, "build", stop_count_increase)
+
+    # Fill the db with "hello-world", "hello-world-wrapper"
+    with bpo_test.BPOServer():
+        bpo_test.trigger.job_callback_get_repo_missing()
+
+        # hello-world: decrease version, change status to failed
+        session = bpo.db.session()
+        pkgname = "hello-world"
+        arch = "x86_64"
+        branch = "master"
+        package = bpo.db.get_package(session, pkgname, arch, branch)
+        package.version = "0-r0"
+        package.status = bpo.db.PackageStatus.failed
+        session.merge(package)
+        session.commit()
+
+        # Fill the db with "hello-world", "hello-world-wrapper" again
+        bpo_test.trigger.job_callback_get_repo_missing()
+
+        # Check if updated properly
+        package = bpo.db.get_package(session, pkgname, arch, branch)
+        assert package.status == bpo.db.PackageStatus.queued
+        assert package.version == "1-r4"
+
+
 def test_callback_repo_missing_to_nop(monkeypatch):
     with bpo_test.BPOServer():
         # Trigger job-callback/get-repo-missing

commit 4171e794b8c0cc4418274ae052023fb9a84ed9c4

cb/get_repo_missing: don't reset all to queued
Do not always set the package status to queued,
only if the version was changed.

Fixes: 106799fa6803ececd1fe2c132b95f2ab7ae69d9c ("callback/get_repo_missing: updated pkg -> queued")

bpo/api/job_callback/get_repo_missing.py
@@ -47,9 +47,9 @@ def update_or_insert_packages(session, payload, arch, branch):
         if package_db:
             if package_db.version != version:
                 bpo.jobs.build_package.abort(package_db)
+                package_db.status = bpo.db.PackageStatus.queued
             package_db.version = version
             package_db.repo = repo
-            package_db.status = bpo.db.PackageStatus.queued
         else:
             package_db = bpo.db.Package(arch, branch, pkgname, version)
         session.merge(package_db)