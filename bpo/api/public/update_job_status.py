fa1387bfcfe4569a055b47a40bf4d9dc8c1448d3
Oliver Smith <ollieparanoid@postmarketos.org>
2019-11-07 23:23:14 +0100

    update-job-status: start next build after failure
    Displaying in the UI that a job has failed is nice, but the real benefit
    is in automatically starting the next job.

--- bpo/api/public/update_job_status.py
+++ bpo/api/public/update_job_status.py
@@ -2,6 +2,7 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
 import bpo.api
 import bpo.helpers.job
+import bpo.repo
 
 blueprint = bpo.api.blueprint
 
@@ -9,4 +10,5 @@ blueprint = bpo.api.blueprint
 @blueprint.route("/api/public/update-job-status", methods=["POST"])
 def public_update_job_status():
     bpo.helpers.job.update_package_status()
+    bpo.repo.build()
     return "done"
