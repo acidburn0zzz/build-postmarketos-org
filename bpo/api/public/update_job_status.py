7de48020940061bd3d2fd05f6e332be82bda0ab1
Oliver Smith <ollieparanoid@postmarketos.org>
2019-11-07 23:44:40 +0100

    Revert "update-job-status: start next build after failure"
    Works great! The problem is however, that Alpine's mirrors are currently
    having problems. So half the packages are gone, and therefore all builds
    are failing :\
    
    Let's revert this patch for now and enable it again tomorrow morning if
    Alpine's repos are back to normal.
    
    This reverts commit fa1387bfcfe4569a055b47a40bf4d9dc8c1448d3.

--- bpo/api/public/update_job_status.py
+++ bpo/api/public/update_job_status.py
@@ -2,7 +2,6 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
 import bpo.api
 import bpo.helpers.job
-import bpo.repo
 
 blueprint = bpo.api.blueprint
 
@@ -10,5 +9,4 @@ blueprint = bpo.api.blueprint
 @blueprint.route("/api/public/update-job-status", methods=["POST"])
 def public_update_job_status():
     bpo.helpers.job.update_package_status()
-    bpo.repo.build()
     return "done"
