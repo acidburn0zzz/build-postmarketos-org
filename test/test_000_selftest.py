ab9843cf9747e76ca9afd12917f2a30cf58a144a
Oliver Smith <ollieparanoid@postmarketos.org>
2020-01-21 22:58:59 +0100

    test_const_pmaports_versions: new test
    Make sure right at the start of the testsuite, that versions in pmaports
    match the bpo_test.const vars. If it does not, explain where to adjust
    it.

--- test/test_000_selftest.py
+++ test/test_000_selftest.py
@@ -1,9 +1,11 @@
 # Copyright 2020 Oliver Smith
 # SPDX-License-Identifier: AGPL-3.0-or-later
 """ Make the testsuite test itself. """
+import os
 import pytest
 
 import bpo_test
+import bpo_test.const
 import bpo_test.trigger
 import bpo.repo
 
@@ -41,3 +43,31 @@ def test_assert_package(monkeypatch):
     with pytest.raises(RuntimeError) as e:
         func(pkgname, exists=False)
         assert str(e.value).startswith("Package should NOT exist")
+
+
+def test_const_pmaports_versions():
+    """ Verify that bpo_test.const.version_* matches pmaports """
+    pmaports_dir = bpo.config.const.top_dir + "/../pmbootstrap/aports"
+    if not os.path.isdir(pmaports_dir):
+        raise RuntimeError("Can't find pmaports, expected them here: '{}'"
+                           " (if you want to add a way to override it, feel"
+                           " free to submit a patch, see #45)"
+                           .format(pmaports_dir))
+    v_hello = bpo_test.const.version_hello_world
+    v_wrapper = bpo_test.const.version_hello_world_wrapper
+    pkgnames_versions = {"hello-world": v_hello,
+                         "hello-world-wrapper": v_wrapper}
+    for pkgname, version in pkgnames_versions.items():
+        pkgver, pkgrel = version.split("-r")
+
+        # Check APKBUILD for pkgver, pkgrel
+        apkbuild_path = "{}/main/{}/APKBUILD".format(pmaports_dir, pkgname)
+        with open(apkbuild_path, "r") as handle:
+            apkbuild = handle.read()
+            for match in ["pkgver=" + pkgver, "pkgrel=" + pkgrel]:
+                if "\n" + match + "\n" in apkbuild:
+                    continue
+                raise RuntimeError("Can't find '{}' line in '{}'. You probably"
+                                   " need to adjust the version_* vars in"
+                                   " test/bpo_test/const.py."
+                                   .format(match, apkbuild_path))
